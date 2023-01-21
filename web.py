e82356bfff056725e219d08915a9c7d30ba8f98e
Arnab55555 <arnabsamanta555@gmail.com>
2023-01-21 17:19:37 +0530

    updates

---
+++ web.py
@@ -0,0 +1,18 @@
+from flask import Flask,render_template, request , redirect , url_for,session
+
+web =Flask(__name__)
+
+@web.route("/")
+def login():
+    return render_template("login.html")
+
+@web.route("/admin_login/")
+def admin_login():
+    return render_template("admin_login.html")
+
+@web.route("/user/")
+def user_login():
+    return render_template("user_login.html")
+
+if __name__=="__main__":
+    web.run(debug=True,port=8000)
