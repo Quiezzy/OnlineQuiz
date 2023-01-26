8d1265ad9acb6c635f751e51430c642b8c73822c
Iliyas-shah <shahiliyas289@gmail.com>
2023-01-27 03:24:16 +0530

    changes

--- db.py
+++ db.py
@@ -1,8 +1,20 @@
 import sqlite3
-def fetch():
-    database_connection = sqlite3.connect("quizitDatabase.db")
-    database_cursor = database_connection.cursor()
-    data=database_cursor.execute("select * from register_admin;")
-    database_connection.commit()
-    database_connection.close()
-    return data
+def adminResult():
+    database_connection=sqlite3.connect("quizitDatabase.db")
+    database_cursor=database_connection.cursor()
+    adminId=int(1)
+    name=database_cursor.execute("select (name) from register_user where userId IN (select (userId) from ansUser where quizId IN (select (quizId) from Quiz where adminId=?))",(adminId,))
+    name=name.fetchall()
+    # quizName=database_cursor.execute("select (quizName) from Quiz where quizId IN (select (quizId) from ansUser where userId IN (select (userId) from ansUser))",(adminId,))
+    quizName=database_cursor.execute("select (quizId) from ansUser where userId IN (select (userId) from ansUser where quizId IN(select (quizId) from Quiz where adminId=?))",(adminId,))
+    quizName=quizName.fetchall()
+    # select (quizName) from Quiz where quizId IN (
+    score=database_cursor.execute("select (result) from ansUser where quizId IN (select (quizId) from Quiz where adminId=?)",(adminId,))
+    score=score.fetchall()
+    total=database_cursor.execute("select (total) from ansUser where quizId IN (select (quizId) from Quiz where adminId=?)",(adminId,))
+    total=total.fetchall()
+    print("name:",name)
+    print("quizname:",quizName)
+    print("score:",score)
+    print("total:",total)
+adminResult()
