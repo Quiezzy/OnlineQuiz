9865567193e48426cebc7e2d756f0abaea28833b
Iliyas-shah <shahiliyas289@gmail.com>
2023-01-27 20:18:54 +0530

    changes

--- db.py
+++ db.py
@@ -5,16 +5,20 @@ def adminResult():
     adminId=int(1)
     name=database_cursor.execute("select (name) from register_user where userId IN (select (userId) from ansUser where quizId IN (select (quizId) from Quiz where adminId=?))",(adminId,))
     name=name.fetchall()
-    # quizName=database_cursor.execute("select (quizName) from Quiz where quizId IN (select (quizId) from ansUser where userId IN (select (userId) from ansUser))",(adminId,))
     quizName=database_cursor.execute("select (quizId) from ansUser where userId IN (select (userId) from ansUser where quizId IN(select (quizId) from Quiz where adminId=?))",(adminId,))
     quizName=quizName.fetchall()
-    # select (quizName) from Quiz where quizId IN (
+    Name=None
+    newName=[]
+    for i in quizName:
+        Name=database_cursor.execute("select (quizName) from Quiz where quizId=?",(i[0],))
+        Name=Name.fetchone()
+        newName.append(Name)
     score=database_cursor.execute("select (result) from ansUser where quizId IN (select (quizId) from Quiz where adminId=?)",(adminId,))
     score=score.fetchall()
     total=database_cursor.execute("select (total) from ansUser where quizId IN (select (quizId) from Quiz where adminId=?)",(adminId,))
     total=total.fetchall()
     print("name:",name)
-    print("quizname:",quizName)
+    print("quizname:",newName)
     print("score:",score)
     print("total:",total)
 adminResult()
--- web.py
+++ web.py
@@ -275,19 +275,26 @@ def adminViewQuiz():
 def adminResult():
     database_connection=sqlite3.connect("quizitDatabase.db")
     database_cursor=database_connection.cursor()
+    adminId=int(1)
     name=database_cursor.execute("select (name) from register_user where userId IN (select (userId) from ansUser where quizId IN (select (quizId) from Quiz where adminId=?))",(adminId,))
     name=name.fetchall()
-    quizName=database_cursor.execute("select (quizName) from Quiz where quizId IN (select (quizId) from ansUser where userId IN (select (userId) from ansUser where quizId IN(select (quizId) from Quiz where adminId=?)))",(adminId,))
+    quizName=database_cursor.execute("select (quizId) from ansUser where userId IN (select (userId) from ansUser where quizId IN(select (quizId) from Quiz where adminId=?))",(adminId,))
     quizName=quizName.fetchall()
+    Name=None
+    newName=[]
+    for i in quizName:
+        Name=database_cursor.execute("select (quizName) from Quiz where quizId=?",(i[0],))
+        Name=Name.fetchone()
+        newName.append(Name)
     score=database_cursor.execute("select (result) from ansUser where quizId IN (select (quizId) from Quiz where adminId=?)",(adminId,))
     score=score.fetchall()
     total=database_cursor.execute("select (total) from ansUser where quizId IN (select (quizId) from Quiz where adminId=?)",(adminId,))
     total=total.fetchall()
     print("name:",name)
-    print("quizname:",quizName)
+    print("quizname:",newName)
     print("score:",score)
     print("total:",total)
-    return render_template("adminResult.html",name=name,quizName=quizName,score=score,total=total)
+    return render_template("adminResult.html",name=name,quizName=newName,score=score,total=total)
 # @web.route("/userResultData/",methods=["POST","GET"])
 # def userResultData():
 #     global userId
